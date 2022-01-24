ce754c7640e8ee8ec672de614307fab179fb7c47
Flippooooo <phschuler@tsn.at>
2022-01-24 19:17:36 +0100

    commit test

---
+++ Tetris.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+pygame.init()
+pygame.display.set_mode((400, 500))
+pygame.display.set_caption("Tetris")
+
+done = False
+while not done:
+    pass
+import time
+time.sleep(5)
+pygame.quit()
+sjbds
