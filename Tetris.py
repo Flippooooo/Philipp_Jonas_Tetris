c3e1113422d83499671b8f37fa043b6ba133eeea
Flippooooo <97949279+Flippooooo@users.noreply.github.com>
2022-01-24 18:40:27 +0100

    Add files via upload

---
+++ Tetris.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+pygame.init()
+pygame.display.set_mode((400, 500))
+pygame.display.set_caption("Tetris")
+
+done = False
+while not done:
+    pass
+import time
+time.sleep(5)
+pygame.quit()
