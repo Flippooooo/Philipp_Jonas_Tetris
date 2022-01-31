e1e6e3cb2dce8e28370ecc29cb754890fdb4efde
Flippooooo <phschuler@tsn.at>
2022-01-31 21:03:22 +0100

    Steuerung

--- Tetris.py
+++ Tetris.py
@@ -1,3 +1,4 @@
+from curses import KEY_DOWN
 import pygame
 
 
@@ -6,8 +7,25 @@ pygame.display.set_mode((400, 500))
 pygame.display.set_caption("Tetris")
 
 done = False
+fps = 25
+clock = pygame.time.Clock()
+counter = 0  
+
+game = Tetris()
+
 while not done:
-    pass
-import time
-time.sleep(5)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                game.rotate() 
+            if event.key == pygame.K_DOWN:
+                game.down()
+            if event.key == pygame.K_LEFT:
+                game.left()
+            if event.key == pygame.K_RIGHT:
+                game.right()
+        if event.key ==     
+
 pygame.quit()
